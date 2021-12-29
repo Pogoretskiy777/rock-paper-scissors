e30193423dee3dc6e2205a50317de58e3d7cb146
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2021-12-29 17:11:20 -0500

    Added playRound function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
-var plays = ['Scissors', 'Rock', 'Paper'];
+const myArray = ["Rock", "Paper", "Scissors"];
 
-var computerPlay = plays[Math.floor(Math.random() * plays.length)];
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}
+var playerSelection = "Rock";
+var computerSelection = computerPlay();
 
-console.log(computerPlay);
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Scissors') {
+    return "You win! Rock beats scissors!";
+  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Rock') {
+    return "A draw! Nice!";
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Paper') {
+    return "You win! Scissors beats paper!";
+  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Scissors') {
+    return "A draw! Nice!"; 
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Rock') {
+    return "You win! Paper beats rock!"
+  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Paper') {
+    return "A draw! Nice!"
+  } else { 
+    return `You lose! ${computerSelection} beats ${playerSelection}!`
+  }
+}
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
