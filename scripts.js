e7de16dfa7d9e33fd3c21b4687c501bff334e077
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2021-12-30 14:52:29 -0500

    Added prompts/alerts

--- scripts.js
+++ scripts.js
@@ -3,25 +3,26 @@ const myArray = ["Rock", "Paper", "Scissors"];
 function computerPlay() {
   return myArray[~~(Math.random() * myArray.length)];
 }
-var playerSelection = "Rock";
+var playerSelection = prompt("Rock, Paper, or Scissors?");
 var computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Scissors') {
-    return "You win! Rock beats scissors!";
+    alert("You win! Rock beats scissors!");
   } else if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Rock') {
-    return "A draw! Nice!";
+    alert("A draw! Nice!");
   } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Paper') {
-    return "You win! Scissors beats paper!";
+    alert("You win! Scissors beats paper!");
   } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Scissors') {
-    return "A draw! Nice!"; 
+    alert("A draw! Nice!"); 
   } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Rock') {
-    return "You win! Paper beats rock!"
+    alert("You win! Paper beats rock!");
   } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Paper') {
-    return "A draw! Nice!"
+    alert("A draw! Nice!");
   } else { 
-    return `You lose! ${computerSelection} beats ${playerSelection}!`
+    alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
   }
 }
+
 console.log(playRound(playerSelection, computerSelection));
 console.log(computerSelection);
