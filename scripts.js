ba2ec3e77b9fdf88e40e7c11964107debb626ee6
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2022-01-03 15:36:53 -0500

    Fixed bugs and refined code

--- scripts.js
+++ scripts.js
@@ -8,36 +8,35 @@ let playerScore = 0;
 let computerScore = 0;
 let score = `Computer: ${computerScore} Player: ${playerScore}`;
 var playerSelection = prompt("Rock, Paper, or Scissors?");
-var computerSelection = computerPlay();
+playerSelection = playerSelection.toLowerCase();
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Scissors') {
+function playRound(playerSelection) {
+  var computerSelection = computerPlay();
+    if (playerSelection === 'rock' && computerSelection==='Scissors') {
     alert("You win! Rock beats scissors!");
-    playerScore + 1;
+    playerScore +=1;
     console.log(score);
-  } else if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Rock') {
-    alert("A draw! Nice!");
-    console.log(score);
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Paper') {
+    console.log(computerSelection);
+  } else if (playerSelection === 'scissors' && computerSelection==='Paper') {
     alert("You win! Scissors beats paper!");
-    playerScore + 1;
+    playerScore +=1;
     console.log(score);
-  } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Scissors') {
-    alert("A draw! Nice!");
-    console.log(score);
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Rock') {
+    console.log(computerSelection);
+  } else if (playerSelection === 'paper' && computerSelection==='Rock') {
     alert("You win! Paper beats rock!");
-    playerScore + 1;
+    playerScore +=1;
     console.log(score);
-  } else if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Paper') {
+    console.log(computerSelection);
+  } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     alert("A draw! Nice!");
     console.log(score);
+    console.log(computerSelection);
   } else { 
     alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
-    computerScore + 1;
+    computerScore += 1;
     console.log(score);
+    console.log(computerSelection);
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(computerSelection);
+playRound(playerSelection);
