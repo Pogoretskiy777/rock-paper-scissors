9b7fafad58680b45db5cd96ce12c560bac140394
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2023-05-18 08:58:07 -0400

    Added game functionality with basic scoring system.

--- scripts.js
+++ scripts.js
@@ -7,45 +7,38 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
+let score = document.getElementById('score-text');
 let buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    console.log(button.textContent);
     let playerSelection = button.textContent.toLowerCase();
+    playRound(playerSelection);
+    score.textContent = `Computer: ${computerScore} Player: ${playerScore}`;
   });
 });
 
-function playRound() {
-  var playerSelection = prompt("Rock, Paper, or Scissors?");
-  playerSelection = playerSelection.toLowerCase();
+
+
+
+function playRound(playerSelection) {
   var computerSelection = computerPlay();
-  let score = `Computer: ${computerScore} Player: ${playerScore}`;
     if (playerSelection === 'rock' && computerSelection==='Scissors') {
     alert("You win! Rock beats scissors!");
     playerScore +=1;
-      console.log(score);
-      console.log(computerSelection);
   } else if (playerSelection === 'scissors' && computerSelection==='Paper') {
     alert("You win! Scissors beats paper!");
     playerScore +=1;
-      console.log(score);
-      console.log(computerSelection);
   } else if (playerSelection === 'paper' && computerSelection==='Rock') {
     alert("You win! Paper beats rock!");
     playerScore +=1;
-      console.log(score);
-      console.log(computerSelection);
   } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     alert("A draw! Nice! No points to either team!");
-      console.log(score);
-      console.log(computerSelection);
   } else if (playerSelection === '') {
       alert("Error");
   } else { 
     alert(`You lose! ${computerSelection} beats ${playerSelection}!`);
     computerScore += 1;
-      console.log(score);
-      console.log(computerSelection);
   }
 }
 
