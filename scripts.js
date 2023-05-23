f84d770b5e79f53d3a8c099c6d0919649cafc24f
Joseph Pogoretskiy <jpogoretskiy@gmail.com>
2023-05-23 17:58:55 -0400

    Improved functionality and javadocs.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const myArray = ["Rock", "Paper", "Scissors"];
 
+/**
+ * This function simulates a computer's play which is completely random out of the three options.
+ *
+ * @returns The computer's play
+ */
 function computerPlay() {
   return myArray[~~(Math.random() * myArray.length)];
 }
@@ -7,48 +12,70 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
-let score = document.getElementById('score-text');
-let buttons = document.querySelectorAll('button');
-let resultContainer = document.querySelector('.result');
-let result = document.createElement('p');
+let score = document.getElementById("score-text");
+let buttons = document.querySelectorAll("button");
+let resultContainer = document.querySelector(".result");
+let result = document.createElement("p");
 resultContainer.appendChild(result);
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    let playerSelection = button.textContent.toLowerCase();
-    let newResult = playRound(playerSelection);
-    score.textContent = `Computer: ${computerScore} Player: ${playerScore}`;
-    if (playerScore == 5 || computerScore == 5) {
-      result.textContent = game();
-    } else {
-      result.textContent = newResult;
-    }
-  });
+  button.addEventListener("click", handleClick);
 });
 
+/**
+ * Every time a decision is clicked, the current score will be updated. When either team reaches 5 points,
+ * the buttons will no longer work.
+ */
+function handleClick() {
+  let playerSelection = this.textContent.toLowerCase();
+  let newResult = playRound(playerSelection);
+  score.textContent = `Computer: ${computerScore} Player: ${playerScore}`;
+  result.textContent = newResult;
+  console.log();
+  if (playerScore === 5 || computerScore === 5) {
+    buttons.forEach((button) => {
+      button.removeEventListener("click", handleClick);
+    });
+    result.textContent = game();
+  }
+}
+
+/**
+ * Add a point to either team depending on the result of the computer's play.
+ *
+ * @param {*} playerSelection The player's selection
+ * @returns The result of the round
+ */
 function playRound(playerSelection) {
   var computerSelection = computerPlay();
   let result;
-    if (playerSelection === 'rock' && computerSelection==='Scissors') {
+  if (playerSelection === "rock" && computerSelection === "Scissors") {
     result = "You win! Rock beats scissors!";
-    playerScore +=1;
-  } else if (playerSelection === 'scissors' && computerSelection==='Paper') {
+    playerScore += 1;
+  } else if (playerSelection === "scissors" && computerSelection === "Paper") {
     result = "You win! Scissors beats paper!";
-    playerScore +=1;
-  } else if (playerSelection === 'paper' && computerSelection==='Rock') {
+    playerScore += 1;
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
     result = "You win! Paper beats rock!";
-    playerScore +=1;
-  } else if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    playerScore += 1;
+  } else if (
+    playerSelection.toLowerCase() === computerSelection.toLowerCase()
+  ) {
     result = "A draw! Nice! No points to either team!";
-  } else if (playerSelection === '') {
+  } else if (playerSelection === "") {
     result = "Error";
-  } else { 
+  } else {
     result = `You lose! ${computerSelection} beats ${playerSelection}!`;
     computerScore += 1;
   }
   return result;
 }
 
+/**
+ * Calculate the game's ending depending on the scores of both parties.
+ *
+ * @returns The result of the game.
+ */
 function game() {
   let score = `Computer: ${computerScore} Player: ${playerScore}`;
   let result;
